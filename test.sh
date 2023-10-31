2570e6b48424a10516de5cc521027e14508e4810
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-31 05:00:27 +0000

    fixes

--- test.sh
+++ test.sh
@@ -9,7 +9,12 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 cd /var/www
 wp plugin install all-in-one-wp-migration --version=7.79 --activate --allow-root --path=/var/www/html
-aws s3 cp ${S3_OBJECT_KEY} all-in-one-wp-migration-unlimited-extension.zip
+aws s3 cp s3://${S3_BUCKET_NAME}/${S3_OBJECT_KEY} all-in-one-wp-migration-unlimited-extension.zip
 wp plugin install all-in-one-wp-migration-unlimited-extension.zip --force --activate --allow-root --path=/var/www/html
 rm all-in-one-wp-migration-unlimited-extension.zip
 chown -R www-data:www-data /var/www/html
+wp ai1wm backup --allow-root --path=/var/www/html
+BACKUP_FOLDER="/var/www/html/wp-content/ai1wm-backups"
+LATEST_BACKUP=$(ls -t ${BACKUP_PATH} | head -n1)
+aws s3 cp ${BACKUP_FOLDER}/${LATEST_BACKUP} s3://${S3_BUCKET_NAME}/${LATEST_BACKUP}
+rm ${BACKUP_FOLDER}/${LATEST_BACKUP}
