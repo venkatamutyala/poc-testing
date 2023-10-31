619ffffdb23925c7d4a50daf96186ce4fc7ee5af
Venkata Mutyala <venkata@venkatamutyala.com>
2023-10-31 05:21:25 +0000

    fixes

--- restore.sh
+++ restore.sh
@@ -2,7 +2,7 @@
 set -e
 
 # download backup and restore
-aws s3 cp s3://${S3_BUCKET_NAME}/${BACKUP_TO_RESTORE} ${BACKUP_FOLDER}/restore.wpress
+aws s3 cp s3://${S3_BUCKET_NAME}/${BACKUP_TO_RESTORE} /var/www/html/wp-content/ai1wm-backups/restore.wpress
 wp ai1wm restore restore.wpress --yes --allow-root --path=/var/www/html
 
-rm ${BACKUP_FOLDER}/restore.wpress
+rm /var/www/html/wp-content/ai1wm-backups/restore.wpress
