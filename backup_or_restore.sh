fd584d3c166625c0df3ec7aa9d2990e3c756857e
Venkata Mutyala <venkata@venkatamutyala.com>
2023-11-01 08:57:25 -0700

    Update backup_or_restore.sh

--- backup_or_restore.sh
+++ backup_or_restore.sh
@@ -2,10 +2,10 @@
 set -e
 
 docker-entrypoint.sh apache2-foreground &
-apt update && apt install awscli -y
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+#apt update && apt install awscli -y
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#mv wp-cli.phar /usr/local/bin/wp
 cd /var/www
 wp plugin install all-in-one-wp-migration --version=7.79 --activate --allow-root --path=/var/www/html
 aws s3 cp s3://${S3_BUCKET_NAME}/${S3_OBJECT_KEY} all-in-one-wp-migration-unlimited-extension.zip
