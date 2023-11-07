18dd77eca9ccd97741f9dff9de46f47c10d0963f
Venkata Mutyala <venkata@venkatamutyala.com>
2023-11-06 16:43:13 -0800

    Update backup_or_restore.sh

--- backup_or_restore.sh
+++ backup_or_restore.sh
@@ -8,7 +8,7 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 cd /var/www
 wp plugin install all-in-one-wp-migration --version=7.79 --activate --allow-root --path=/var/www/html
-aws s3 cp s3://${S3_BUCKET_NAME}/${S3_OBJECT_KEY} all-in-one-wp-migration-unlimited-extension.zip
+aws s3 cp s3://${S3_BUCKET_NAME}/${S3_OBJECT_KEY_FOR_PLUGIN_ZIP} all-in-one-wp-migration-unlimited-extension.zip
 wp plugin install all-in-one-wp-migration-unlimited-extension.zip --force --activate --allow-root --path=/var/www/html
 rm all-in-one-wp-migration-unlimited-extension.zip
 chown -R www-data:www-data /var/www/html
@@ -16,13 +16,13 @@ chown -R www-data:www-data /var/www/html
 if [ -n "$BACKUP_TO_RESTORE" ]; then
     echo "RESTORING BACKUP NOW: ${BACKUP_TO_RESTORE}"
     # download backup and restore
-    aws s3 cp s3://${S3_BUCKET_NAME}/${BACKUP_TO_RESTORE} /var/www/html/wp-content/ai1wm-backups/restore.wpress
+    aws s3 cp s3://${S3_BUCKET_NAME}/backups/${BACKUP_TO_RESTORE} /var/www/html/wp-content/ai1wm-backups/restore.wpress
     wp ai1wm restore restore.wpress --yes --allow-root --path=/var/www/html
     rm /var/www/html/wp-content/ai1wm-backups/restore.wpress
 else
     wp ai1wm backup --allow-root --path=/var/www/html --exclude-spam-comments --exclude-post-revisions --exclude-themes --exclude-inactive-themes --exclude-muplugins --exclude-inactive-plugins --exclude-cache --exclude-email-replace 
     LATEST_BACKUP=$(ls -t /var/www/html/wp-content/ai1wm-backups | head -n1)
-    aws s3 cp /var/www/html/wp-content/ai1wm-backups/${LATEST_BACKUP} s3://${S3_BUCKET_NAME}/${LATEST_BACKUP}
+    aws s3 cp /var/www/html/wp-content/ai1wm-backups/${LATEST_BACKUP} s3://${S3_BUCKET_NAME}/backups/${LATEST_BACKUP}
     rm /var/www/html/wp-content/ai1wm-backups/${LATEST_BACKUP}
     echo "BACKUP NAME: ${LATEST_BACKUP}"
 fi
